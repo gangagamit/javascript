a43cd0f50774f6d60b3750abc16f5c1b6580bc36
gangagamit <gangagamit350@gmail.com>
2024-02-28 09:21:26 +0530

    28-2-2024

--- javascript/array-method/p1.js
+++ javascript/array-method/p1.js
@@ -43,13 +43,12 @@ function uppercase(str) {
 Example string : 'Web Development Tutorial'
 Expected Output : 'Development'*/
 {
-  Longword( 'Web Development Tutorial'); 
+  Longword( 'Web Development Tutorial '); 
   function Longword(str){ 
       let words = str.split(' '); 
       console.log(words); 
       let maxLength = 0; 
       let longword = ""; 
-     
       for (let i = 0; i < words.length; i++) 
        { 
         if (words[i].length > maxLength) { 
@@ -61,3 +60,22 @@ Expected Output : 'Development'*/
   }
 }
 /*5. Write a JavaScript function that checks whether a number is perfect.*/
+
+
+let perfect = (a) =>{
+  let n = 0;
+  for(let i = 1; i < a; i++){
+      if(a % i == 0)
+      {
+          n = n + i;
+          console.log(i);
+      }
+  }
+  console.log(n);
+  if(a == n){
+      console.log("number is perfact");
+  }else{
+      console.log("number is not perfact");
+  }
+}
+perfect(7);
