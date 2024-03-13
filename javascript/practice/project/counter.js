a4b7a7ab2e0c90f2a7ef724ece434372d137b2ce
gangagamit <gangagamit350@gmail.com>
2024-03-13 20:59:03 +0530

    13-3-2024

--- javascript/practice/project/counter.js
+++ javascript/practice/project/counter.js
@@ -1,3 +1,11 @@
 {
-    
+       
+function wordsLen(str) { 
+    const array = str.trim().split(/\s+/); 
+    return array.length; 
+} 
+  
+const str = "lorem  wjdyuiwqd qwduhwqiudbjsakdsjadjh wqjhjdgqydw qysgsyjsbx  wjdh asdh wshds hasv"; 
+  
+console.log("Word count:" ,wordsLen(str));
 }
